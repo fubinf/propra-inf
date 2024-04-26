53f022ece926dad770d96a601a72ed0bc71e6626
Dominik Pietrak <dom.pietrak@gmail.com>
2024-04-26 11:40:45 +0200

    ch/Debugging/Häufige-Defektarten/Off-by-1-Defekte.py: adapted reworked Python code from Prechelt for a more pythonic feel

--- ch/Debugging/Häufige-Defektarten/Off-by-1-Defekte.py
+++ ch/Debugging/Häufige-Defektarten/Off-by-1-Defekte.py
@@ -1,19 +1,24 @@
+import dataclasses
+
+
+@dataclasses.dataclass
 class Month:
     name: str
     days: int
 
+    def __repr__(self):
+        return f"{self.name}({self.days})"
+
 
 def showday(daynumber: int, isleapyear: bool) -> (str, int):
-    """Shows the month and day for a given day number.
-
+    """
+    Returns the month and day for a given day number.
     daynumber: a day number within the year.
     isleapyear: True if the year is a leap year.
-
-    Returns the month and day,
+    Returns the month name and day,
     raises ValueError if daynumber is invalid.
     """
-
-    months = (
+    monthnames = (
         "January",
         "February",
         "March",
@@ -23,40 +28,22 @@ def showday(daynumber: int, isleapyear: bool) -> (str, int):
         "July",
         "August",
         "September",
+        "October",
         "November",
         "December",
     )
-
-    days = [31 for month in months]
-
-    # Let's see, 30 days hath September...
-
+    days = [31 for month in monthnames]  # assume all months have 31 days
     thirtylist = ("April", "June", "September", "November")
-
-    for j in [months.index(k) for k in thirtylist]:
-        days[j] = 30
-
-    # Fix up February also
-
-    days[months.index("February")] = 28 + isleapyear
-
-    """ daymap consists of 12 Month objects, each of which has a name/days pair in it
-    """
-
-    daymap = []
-
-    for i in range(len(months)):
-        NewMonth = Month()
-        NewMonth.name = months[i]
-        NewMonth.days = days[i]
-        daymap.append(NewMonth)
-
+    for idx in [monthnames.index(thismonth) for thismonth in thirtylist]: # fix 30-day months
+        days[idx] = 30
+    days[monthnames.index("February")] = 28 + isleapyear  # fix February
+    months = [Month(name=monthnames[i], days=days[i]) for i in range(len(monthnames))]  # our target data structure
+    # now do the computation:
     if daynumber > 0:
-        for el in daymap:
-            if daynumber < el.days:
-                return el.name, daynumber
-            daynumber = daynumber - el.days
-    raise ValueError("daynumber")
-
+        for month in months:
+            if daynumber < month.days:  # the right month
+                return month.name, daynumber
+            daynumber = daynumber - month.days  # not the right month, subtract it and continue
+    raise ValueError("invalid daynumber")
 
 print(showday(1, False))
