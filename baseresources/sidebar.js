d180fe4a3289de5e04114af786ad3d20db4bcf44
myname <myname@mynery.eu>
2024-10-21 18:57:39 +0200

    implement basic glossary search

--- baseresources/sidebar.js
+++ baseresources/sidebar.js
@@ -19,7 +19,7 @@ if (filename) {
 }
 const timer = document.getElementsByClassName("breadcrumbs")?.[0]
   ?.parentElement?.appendChild(document.createElement("span"));
-if (timer) {
+if (timer && filename != "glossary") {
   timer.id = "timer";
   let time = 0, start;
   const controls = Object.fromEntries(["time", "copy", "play", "pause", "stop"].map(c => [c, timer.appendChild(document.createElement("span"))]));
@@ -58,4 +58,49 @@ if (timer) {
     time = 0;
     playPause(false);
   }
+}
+
+function distance(a, b) {
+    let m = [], i, j, min = Math.min;
+
+    if (!(a && b)) return (b || a).length;
+
+    for (i = 0; i <= b.length; m[i] = [i++]);
+    for (j = 0; j <= a.length; m[0][j] = j++);
+
+    for (i = 1; i <= b.length; i++) {
+        for (j = 1; j <= a.length; j++) {
+            m[i][j] = b.charAt(i - 1) == a.charAt(j - 1)
+                ? m[i - 1][j - 1]
+                : m[i][j] = min(
+                    m[i - 1][j - 1] + 1, 
+                    min(m[i][j - 1] + 1, m[i - 1 ][j] + 1))
+        }
+    }
+
+    return m[b.length][a.length];
+}
+
+const main = document.getElementsByTagName("main")?.[0]?.firstElementChild;
+const searchContainer = main?.appendChild?.(document.createElement("div"));
+if (searchContainer && filename == "glossary") {
+  const terms = Object.fromEntries([...document.getElementsByClassName("glossary-term-block")]
+    .flatMap(e => e.getElementsByClassName("glossary-term-heading")[0].innerText.split(" | ").map(h => [h, e.firstElementChild.id])));
+  searchContainer.style.float = "right";
+  const searchText = document.createElement("input");
+  searchText.type = "text";
+  searchText.placeholder = "🔍";
+  const searchResults = document.createElement("datalist");
+  searchResults.id = "searchResults";
+  searchText.setAttribute("list", searchResults.id);
+  searchText.addEventListener("input", e => {
+    searchResults.innerHTML = "";
+    const input = e.target.value.toLowerCase();
+    Object.keys(terms).map(t => [t, t.toLowerCase()]).filter(t => t[1].includes(input))
+      .map(t => [t[0], distance(t[1], input)]).sort((a, b) => a[1] - b[1]).slice(0, 10)
+      .forEach(match => { searchResults.appendChild(document.createElement("option")).value = match[0]; });
+  });
+  searchText.addEventListener("change", e => { location.hash = "#" + terms[e.target.value]; });
+  searchContainer.appendChild(searchText);
+  searchContainer.appendChild(searchResults);
 }
