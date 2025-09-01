4c76a4813561e6fd710b7e147d47807c7eba42e3
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2025-09-01 10:54:50 +0200

    progressplot.py: introduced 'released' plot; refined good/OK/failure lines

--- process/progressplot.py
+++ process/progressplot.py
@@ -13,11 +13,23 @@ def plot_progress(df: pd.DataFrame, columns: list[str]):
     for col in columns:
         plt.plot(x, df.loc[:, col], label=col)
     # ----- helper lines:
-    plt.axhline(y=400, color='darkgreen', linestyle='dotted', linewidth=2, label='Good')
-    plt.axhline(y=300, color='darkblue', linestyle='dotted', linewidth=2, label='OK')
-    plt.axhline(y=150, color='darkred', linestyle='dotted', linewidth=2, label='Failure')
-    plt.plot(pd.to_datetime(['2024-01-01', '2024-04-01']), [0, 400], color='black', linestyle='dashed', linewidth=2)
-    plt.plot(pd.to_datetime(['2024-04-01', '2025-03-31']), [400, 400], color='black', linestyle='dashed', linewidth=2)
+    sem0start = '2024-04-01'  # pilot course first-and-only semester
+    sem0end = '2024-09-30'
+    sem1start = '2025-04-01'  # first regular course first semester
+    sem1end = '2025-09-30'
+    sem2end = '2026-03-31'
+    # plt.axhline(y=400, color='darkgreen', linestyle='dotted', linewidth=2, label='Good')
+    # plt.axhline(y=300, color='darkblue', linestyle='dotted', linewidth=2, label='OK')
+    plt.plot(pd.to_datetime([sem1start, sem2end]), [400, 400],
+                color='darkgreen', linestyle='dotted', linewidth=2, label='Good')
+    plt.plot(pd.to_datetime([sem0start, sem2end]), [300, 300],
+                color='darkblue', linestyle='dotted', linewidth=2, label='OK')
+    plt.plot(pd.to_datetime([sem0start, sem1end]), [150, 150],
+                color='darkred', linestyle='dotted', linewidth=2, label='Failure')
+    plt.plot(pd.to_datetime(['2024-01-01', '2024-04-01']), [0, 400], 
+             color='black', linestyle='dashed', linewidth=2)
+    # plt.plot(pd.to_datetime(['2024-04-01', '2024-09-30']), [400, 400], 
+    #          color='black', linestyle='dashed', linewidth=2)
     # ----- decoration:
     plt.xlabel('date')
     plt.ylabel('timevalue')
@@ -28,11 +40,12 @@ def plot_progress(df: pd.DataFrame, columns: list[str]):
 inputfile = sys.argv[1]
 outputfile = sys.argv[2]  # we do no checking 
 df = pd.read_csv(inputfile)
-df['total'] = df['alpha'] + df['beta'] + df['done']
+df['released'] = df['beta'] + df['done']
+df['total'] = df['alpha'] + df['released']
 pdf = mbbp.PdfPages(outputfile)
 plt.figure(figsize=(15, 6))  # Landscape orientation
 
-plot_progress(df, ["alpha", "beta", "done", "total"])
+plot_progress(df, ["alpha", "beta", "done", "released"])
 
 pdf.savefig()
 plt.close()
